65c5ee739148be4a23849f133e974b2379c2f101
a71143055 <a71143055@gmail.com>
2024-10-14 10:31:12 +0900

    midterm_project

---
+++ src/BookDTO.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class BookDTO {
+    String isbn;
+    String bookName;
+    String author;
+    String publish;
+    int price;
+
+    public ArrayList<BookDTO> books = new ArrayList<BookDTO>();
+}
